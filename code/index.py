6a43464977a41ca36b2e91b7f86aabaa6a420725
elarcanista <elarcanista@gmail.com>
2022-10-15 18:44:05 -0500

    Refactor Confusion Matrix

--- code/index.py
+++ code/index.py
@@ -13,44 +13,31 @@ def PAC_delta(H_norm, epsilon, eta):
 # FN TP
 def confusion(U1, U2, supervise = True, compact=True):
     p = U1.shape[1]
-    if supervise:
-        U1 = np.argmax(U1, axis = 0)
-        U2 = np.argmax(U2, axis = 0)
-        CM = np.zeros((np.max(U1)+1, np.max(U1)+1))
-        for u in range(p):
-            CM[U2[u], U1[u]] += 1
-        return CM/p
-    else:
-        # Compares if pairs of points share cluster under U1 and U2 or not
-        U1 = np.argmax(U1, axis = 0, keepdims=True) != 0
-        U2 = np.argmax(U2, axis=0, keepdims=True) != 0
+    A = np.argmax(U1, axis = 0, keepdims=True) != 0
+    B = np.argmax(U2, axis=0, keepdims=True) != 0
+    if not supervise:
         equal_pw = lambda A: ~(A.T @ ~A) * ~(~A.T @ A)
-        D1 = equal_pw(U1)
-        D2 = equal_pw(U2)
-        CM_T = np.array([
-            [np.sum(~D1 * ~D2), np.sum(D1 * ~D2)],
-            [np.sum(~D1 * D2), np.sum(D1 * D2) - U1.shape[1]]])
-        return CM_T / 2
-
+        A = equal_pw(U1)
+        B = equal_pw(U2)
+    CM = np.array([
+            [np.sum(~A * ~B), np.sum(A * ~B)],
+            [np.sum(~A * B), np.sum(A * B)]])
+    if not supervise:
+        CM[1, 1] -= p
+        CM /= 2
+    return CM
 
 
 index = {
-    "P": lambda CM: #Precision
-        CM[0,0] / (CM[0,0] + CM[1,0]),
-    "R": lambda CM: #Recall
-        CM[0, 0] / (CM[0, 0] + CM[0, 1]),
-    "F": lambda alpha: lambda CM: #F-alpha
-        (1 + alpha) * index["P"](CM) * index["R"](CM) /
-        (alpha * index["P"](CM) + index["R"](CM)),
-    "Mu": lambda CM: #Mean
-        (CM[0,0] + CM[0,1]) / np.sum(CM),
-    "Sigma2": lambda CM: #Variance
-        index["Mu"](CM) - index["Mu"](CM)**2,
-    "Phi": lambda CM: #Phi
-        (CM[0,0] * CM[1,1] - CM[0,1] * CM[1,0]) /
-        ((CM[0, 0] + CM[0, 1]) * (CM[0, 0] + CM[1, 0])
-         * (CM[0, 1] + CM[1, 1]) * (CM[1, 0] + CM[1, 1])),
-    "Rand": lambda CM: #Rand
-        (CM[0,0] + CM[1,1])/np.sum(CM)
+    "Sensitivity": lambda TN, FP, FN, TP: #Sensitivity
+        TP/(TP + FN),
+    "Specificity": lambda TN, FP, FN, TP: #Specificity
+        TN/(TN + FP),
+    "Precision": lambda TN, FP, FN, TP: #Precision
+        TP/(TP + FP),
+    "Accuracy": lambda TN, FP, FN, TP: #Accuracy
+        (TP + TN) / (TN+FP+FN+TP),
+    "Phi": lambda TN, FP, FN, TP: #Phi
+        ((TP*TN) - (FP*FN)) / np.sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
 }
 
