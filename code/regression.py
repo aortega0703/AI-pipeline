b8a2fd171086f1e6e39ab1842093162e4214ada9
elarcanista <elarcanista@gmail.com>
2022-10-15 15:12:40 -0500

    Fix Regression orientation

--- code/regression.py
+++ code/regression.py
@@ -5,30 +5,29 @@ from numpy.linalg import inv
 
 
 def linear(X, Y):
-    X = np.concatenate([np.ones((X.shape[1], 1)), X.T], axis=1)
-    Y = Y.T
-    B = inv(X.T @ X) @ X.T @ Y
-    E = norm_space.norm["Euclidean2"](Y - (X @ B))
-    return B, E
+    X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
+    B = Y @ X.T @ inv(X @ X.T)
+    E = norm_space.norm["Euclidean2"](Y - (B @ X))
+    return B, np.sum(E)
 
 
 def linear_eval(X, B):
-    X = np.concatenate([np.ones((X.shape[1], 1)), X.T], axis=1)
-    return (X @ B).T
+    X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
+    return B @ X
 
 
-def logistic(X, Y):
-    P = (P.T * 0.98) + 0.01
-    #Y = np.log(P/(1-P))
-    B, E = linear(X, Y.T)
-    X = np.concatenate([np.ones((X.shape[1], 1)), X.T], axis=1)
-    Y = X @ B
-    Yd = 1/(1+np.exp(-Y))
-    E = norm_space.norm["Euclidean2"](P - Yd)
-    return B, E
+def logistic(X, P):
+    P = (P * 0.98) + 0.01
+    Y = np.log(P/(1-P))
+    B, E = linear(X, Y)
+    X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
+    Y_out = B @ X
+    P_out = 1/(1+np.exp(-Y_out))
+    E = norm_space.norm["Euclidean2"](P - P_out)
+    return B, np.sum(E)
 
 
 def logistic_eval(X, B):
-    X = np.concatenate([np.ones((X.shape[1], 1)), X.T], axis=1)
-    Y = (X @ B)
-    return (1/(1+np.exp(-Y))).T
+    X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
+    Y = B @ X
+    return 1/(1+np.exp(-Y))
