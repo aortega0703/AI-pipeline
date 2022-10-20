b6a67bfec339dc4aa1e3e9d19365881d0c78ba78
elarcanista <elarcanista@gmail.com>
2022-10-20 08:04:23 -0500

    SVM Kernels

--- code/SVM.py
+++ code/SVM.py
@@ -1,10 +1,22 @@
 import numpy as np
 
-def train(X, U, epochs, tolerance, eta=1):
+kernel = {
+    "Identity" :
+        lambda X, Y:
+            X.T @ Y,
+    "Polynomial" : 
+        lambda d, c: lambda X, Y:
+            (X.T @ Y + c)**d,
+    "Radial":
+        lambda gamma: lambda X, Y:
+            np.squeeze(np.exp(-gamma *
+                np.linalg.norm(X.T[:, None, :] - Y.T[None,:,:], axis=2)))
+}
+
+def train(X, U, epochs, tolerance, eta=1, K=kernel["Identity"]):
     Y = np.argmax(U, axis=0, keepdims=True).T*2-1
-    dots = (Y * X.T) @ (X * Y.T)
+    dots = Y * K(X, X) * Y.T
     alpha = np.random.rand(X.shape[1], 1)
-    b = np.random.rand()
     alpha_norm = []
     cost = []
     for e in range(epochs):
@@ -19,8 +31,11 @@ def train(X, U, epochs, tolerance, eta=1):
         if (e+1) % (epochs*0.1) == 0:
             print(e+1)
     W = np.sum(X * alpha.T * Y.T, axis=1, keepdims=True)
-    return W, b, cost, alpha_norm
+    return W, alpha, cost, alpha_norm
 
-def eval(X, W, Yd=None):
-    Y = W.T @ X <= 0
-    return np.concatenate([Y, ~Y], axis=0)
+def eval(U, alpha, X, Y, Yd=None, K=kernel["Identity"]):
+    Y = np.argmax(Y, axis=0, keepdims=True).T*2-1
+    print(U.shape, Y.shape)
+    out = np.sum(alpha * Y * K(X, U), axis=0, keepdims=True) <= 0
+    print(out.shape)
+    return np.concatenate([out, ~out], axis=0)
