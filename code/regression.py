f59654db62c78565c8c88d3cb77c244102172722
elarcanista <elarcanista@gmail.com>
2022-11-14 14:00:18 -0500

    Regression documentation

--- code/regression.py
+++ code/regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Types of regression and their inverses
 regression = {
     "Linear": (
         lambda Y: Y,
@@ -11,13 +12,14 @@ regression = {
     )
 }
 
+# Fits the desired regression model to the XY data given.
 def train(X, Y, k = regression["Linear"]):
     X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
     Y = k[0](Y*0.98 + 0.01)
     B = Y @ X.T @ np.linalg.inv(X @ X.T)
     return B
 
-
+# given a regression model and a list of points, evaluates the model on each.
 def eval(X, B, k = regression["Linear"]):
     X = np.concatenate([np.ones((1, X.shape[1])), X], axis=0)
     Y = B @ X
