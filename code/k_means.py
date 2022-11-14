4dd24679fd4b68251dcb46b8d7c8716f6affe1f8
elarcanista <elarcanista@gmail.com>
2022-11-14 12:08:14 -0500

    K-means documentation

--- code/k_means.py
+++ code/k_means.py
@@ -2,12 +2,16 @@ import norm_space
 
 import numpy as np
 
+# Given a list of points and classifications, calculates the centers for each
+# class.
 def update_centers(X, U):
     C = np.zeros((X.shape[0], U.shape[0]))
     for c, u in enumerate(U):
         C[:, c] = np.average(X[:, u == 1], axis=1)
     return C
 
+# Given a list of points and centers, assigns one of the centers to each point
+# based on their proximity.
 def eval(X, C, norm=norm_space.norm["Euclidean2"]):
     U = np.zeros((C.shape[1], X.shape[1]), dtype=int)
     D = norm_space.dist_matrix(C, X, norm)
@@ -16,7 +20,8 @@ def eval(X, C, norm=norm_space.norm["Euclidean2"]):
         U[:, x] = I[:, c]
     return U
 
-
+# Runs the k-means algorithm on a list of points until a given threshold of the
+# cost funtion is reached.
 def train(X, k, epsilon, norm=norm_space.norm["Euclidean2"], cost=None):
     if cost is None:
         def cost(X, U, C, norm=norm_space.norm["Euclidean2"]):
